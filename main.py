b7535e608a492fe2665c617dfda68feeb80d32f8
Misha-Mayskiy <shofer2007@gmail.com>
2024-01-17 19:00:25 +0500

    v1.01

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
-import random
-from collections import Counter
-
-
 def create_random_list(length, min_value, max_value):
+    import random
     return [random.randint(min_value, max_value) for _ in range(length)]
 
 
 def dict_generator(my_list):
+    from collections import Counter
     return dict(Counter(my_list))
 
 
 def main():
     mn, mx, ln = 1, 10, 20
-
     lst = create_random_list(ln, mn, mx)
     dct = dict_generator(lst)
     print(dct)
